5ed62fedd6647d78193836b07a0fe44988470ccd
Kenan Bitikofer <kbitikofer@gmail.com>
2017-12-13 13:01:54 -0500

    Prevent overwriting of room_strategy on upgrade

--- memory_set.js
+++ memory_set.js
@@ -8,7 +8,8 @@ module.exports = {
     //This is a good method to call for the first ever room, to get everything up and running
     // e.g. require('memory_set').controller1_basic('E27S26')
     controller1_basic: function(roomName){
-        Memory.room_strategy[roomName]={
+        if (! Memory.room_strategy[roomName]) Memory.room_strategy[roomName] = {}
+        var updates = {
             'spawn_priority': ['role_harvester', 'role_guard', 'role_upgrader', 'role_builder', 'role_claimer' ],
             'role_harvester': {'desired_number':1, 'parts':[MOVE,WORK,CARRY] },
             'role_guard': {'desired_number':1, 'parts':[MOVE,ATTACK,TOUGH,TOUGH] },
@@ -16,12 +17,16 @@ module.exports = {
             'role_builder': {'desired_number':3, 'parts':[MOVE,WORK,CARRY] },
             'role_solominer': {'parts':[WORK,WORK,WORK,WORK,WORK,MOVE]} //Don't make this until you're shifting to the next
         }
+        Object.keys(updates).forEach(function(key){
+            Memory.room_strategy[roomName][key] = updates[key]
+        });
     },
 
     //Once you have lvl2, 5 extensions, and container(s) by the source(s)
     //When switching to this, make sure you place a 'solomine*' flag to create the solominer(s)!
     controller2_solomining: function(roomName){
-        Memory.room_strategy[roomName]={
+        if (! Memory.room_strategy[roomName]) Memory.room_strategy[roomName] = {}
+        var updates = {
             'spawn_priority': ['role_restocker', 'role_solominer', 'role_guard', 'role_upgrader', 'role_builder', 'role_claimer' ],
             'role_harvester': {'desired_number':0, 'parts':[MOVE,WORK,CARRY] },
             'role_restocker': {'desired_number':2, 'parts':[MOVE, CARRY, CARRY] },
@@ -30,22 +35,30 @@ module.exports = {
             'role_builder': {'desired_number':3, 'parts':[MOVE, MOVE, WORK,WORK, CARRY,CARRY] },
             'role_solominer': {'parts':[WORK,WORK,WORK,WORK,WORK, MOVE]} //You need 5 extensions before you can make this one
         }
+        Object.keys(updates).forEach(function(key){
+            Memory.room_strategy[roomName][key] = updates[key]
+        });
     },
 
     //When wanting to claim a new room and send workers there, call this on the new room
     // E.g. require('memory_set').bootstrap_room('E28S22', 'E27S24')
     bootstrap_room: function(roomName, fromRoom){
-        Memory.room_strategy[roomName]={
+        if (! Memory.room_strategy[roomName]) Memory.room_strategy[roomName] = {}
+        var updates = {
             'spawn_priority':['role_claimer', 'role_builder'],
             'role_claimer':{'spawn_room':fromRoom},
             'role_builder':{'spawn_room':fromRoom, 'parts':[MOVE,MOVE,MOVE, WORK,WORK,WORK, CARRY,CARRY,CARRY], 'desired_number':1}
         }
+        Object.keys(updates).forEach(function(key){
+            Memory.room_strategy[roomName][key] = updates[key]
+        });
     },
 
     //When wanting to claim a new room and send workers there, call this on the new room
     // E.g. require('memory_set').remote_mine_room('E28S26', 'E27S26')
     remote_mine_room: function(roomName, fromRoom){
-        Memory.room_strategy[roomName]={
+        if (! Memory.room_strategy[roomName]) Memory.room_strategy[roomName] = {}
+        var updates = {
             'spawn_priority':['role_guard','role_solominer', 'role_claimer', 'role_builder'],
             'role_claimer':{'spawn_room':fromRoom},
             'role_builder':{'spawn_room':fromRoom, 'parts':[MOVE,MOVE,MOVE, WORK,WORK,WORK, CARRY,CARRY,CARRY], 'desired_number':1},
@@ -53,13 +66,17 @@ module.exports = {
             'role_trucker':{'spawn_room':fromRoom, 'parts':[MOVE,MOVE,MOVE,MOVE,MOVE,MOVE, WORK, CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY,CARRY]},
             'role_guard':    {'spawn_room':fromRoom, 'desired_number':1, 'parts':[TOUGH,TOUGH,TOUGH,TOUGH, MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK,ATTACK,ATTACK] },
         }
+        Object.keys(updates).forEach(function(key){
+            Memory.room_strategy[roomName][key] = updates[key]
+        });
     },
     
     
 
     //For controller level 3. Solomining is assumed by this point. Prioritize building the new extensions and a tower
     controller3: function(roomName){
-        Memory.room_strategy[roomName]={
+        if (! Memory.room_strategy[roomName]) Memory.room_strategy[roomName] = {}
+        var updates = {
             'spawn_priority': ['role_restocker', 'role_solominer', 'role_guard', 'role_upgrader', 'role_builder', 'role_claimer' ],
             'role_harvester':{'desired_number':0, 'parts':[MOVE,WORK,CARRY] },
             'role_restocker':{'desired_number':2, 'parts':[MOVE, CARRY,CARRY] },
@@ -68,12 +85,16 @@ module.exports = {
             'role_builder':  {'desired_number':3, 'parts':[MOVE,MOVE,MOVE, WORK,WORK,WORK, CARRY,CARRY,CARRY] },
             'role_solominer':{'parts':[WORK,WORK,WORK,WORK,WORK, MOVE]}
         }
+        Object.keys(updates).forEach(function(key){
+            Memory.room_strategy[roomName][key] = updates[key]
+        });
     },
 
 
     //For controller level 3, to minimize the creep count for CPU
     controller3_minimal: function(roomName){
-        Memory.room_strategy[roomName]={
+        if (! Memory.room_strategy[roomName]) Memory.room_strategy[roomName] = {}
+        var updates = {
             'spawn_priority': ['role_restocker', 'role_solominer', 'role_guard', 'role_upgrader', 'role_builder', 'role_claimer' ],
             'role_harvester':{'desired_number':0, 'parts':[MOVE,WORK,CARRY] },
             'role_restocker':{'desired_number':1, 'parts':[MOVE,MOVE, CARRY,CARRY,CARRY,CARRY] },
@@ -82,11 +103,15 @@ module.exports = {
             'role_builder':  {'desired_number':2, 'parts':[MOVE,MOVE,MOVE,MOVE, WORK,WORK,WORK, CARRY,CARRY,CARRY,CARRY] },
             'role_solominer':{'parts':[WORK,WORK,WORK,WORK,WORK, MOVE]}
         }
+        Object.keys(updates).forEach(function(key){
+            Memory.room_strategy[roomName][key] = updates[key]
+        });
     },
 
     //For controller level 4, to minimize the creep count for CPU
     controller4: function(roomName){
-        Memory.room_strategy[roomName]={
+        if (! Memory.room_strategy[roomName]) Memory.room_strategy[roomName] = {}
+        var updates = {
             'spawn_priority': ['role_restocker', 'role_solominer', 'role_guard', 'role_upgrader', 'role_builder', 'role_claimer' ],
             'role_harvester':{'desired_number':0, 'parts':[MOVE,WORK,CARRY] },
             'role_restocker':{'desired_number':2, 'parts':[MOVE,MOVE, CARRY,CARRY,CARRY,CARRY] },
@@ -95,11 +120,15 @@ module.exports = {
             'role_upgrader':  {'desired_number':1, 'parts':[MOVE,MOVE, WORK,WORK,WORK,WORK,WORK,WORK,WORK, CARRY] },
             'role_solominer':{'parts':[WORK,WORK,WORK,WORK,WORK, MOVE]}
         }
+        Object.keys(updates).forEach(function(key){
+            Memory.room_strategy[roomName][key] = updates[key]
+        });
     },
 
     //For controller level 5. Links are now assumed. This lightweight setup should help you fill up a storage with energy
     controller5: function(roomName){
-        Memory.room_strategy[roomName]={
+        if (! Memory.room_strategy[roomName]) Memory.room_strategy[roomName] = {}
+        var updates = {
             'spawn_priority': ['role_restocker', 'role_solominer', 'role_guard', 'role_upgrader', 'role_builder', 'role_claimer' ],
             'role_harvester':{'desired_number':0, 'parts':[MOVE,WORK,CARRY] },
             'role_restocker':{'desired_number':1, 'parts':[MOVE,MOVE,MOVE, CARRY,CARRY,CARRY,CARRY,CARRY,CARRY] },
@@ -108,6 +137,26 @@ module.exports = {
             'role_upgrader':  {'desired_number':1, 'parts':[MOVE,MOVE, WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK, CARRY] },
             'role_solominer':{'parts':[WORK,WORK,WORK,WORK,WORK, MOVE, CARRY]}
         }
+        Object.keys(updates).forEach(function(key){
+            Memory.room_strategy[roomName][key] = updates[key]
+        });
+    },
+
+    //For controller level 6. The motions of creeps should be reduced as much as possible.
+    controller6: function(roomName){
+        if (! Memory.room_strategy[roomName]) Memory.room_strategy[roomName] = {}
+        var updates = {
+            'spawn_priority': ['role_restocker', 'role_solominer', 'role_guard', 'role_upgrader', 'role_builder', 'role_claimer' ],
+            'role_harvester':{'desired_number':0, 'parts':[MOVE,WORK,CARRY] },
+            'role_restocker':{'desired_number':1, 'parts':[MOVE,MOVE,MOVE, CARRY,CARRY,CARRY,CARRY,CARRY,CARRY] },
+            'role_guard':    {'desired_number':1, 'parts':[TOUGH,TOUGH,TOUGH,TOUGH, MOVE,MOVE,MOVE,MOVE,ATTACK,ATTACK,ATTACK,ATTACK] },
+            'role_builder':  {'desired_number':1, 'parts':[MOVE,MOVE,MOVE,MOVE, WORK,WORK,WORK,WORK,WORK,WORK, CARRY,CARRY] },
+            'role_upgrader':  {'desired_number':1, 'parts':[MOVE,MOVE, WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK,WORK, CARRY] },
+            'role_solominer':{'parts':[WORK,WORK,WORK,WORK,WORK, MOVE, CARRY]}
+        };
+        Object.keys(updates).forEach(function(key){
+            Memory.room_strategy[roomName][key] = updates[key]
+        });
     },
 
     //Example: require('memory_set').storage(Game.getObjectById('5a2472801369731ed9a3ca7e'))
